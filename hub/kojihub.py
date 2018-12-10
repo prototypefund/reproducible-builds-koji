84df1d078c179a35942aae5e40ea75995e405cdd
Mike McLean <mikem@redhat.com>
2018-12-10 12:06:50 -0500

    fix arches handling in listHosts

--- hub/kojihub.py
+++ hub/kojihub.py
@@ -10574,8 +10574,8 @@ class RootExports(object):
             # matching 'ppc64'
             if not (isinstance(arches, list) or isinstance(arches, tuple)):
                 arches = [arches]
-            archClause = [r"""arches ~ E'\\m%s\\M'""" % arch for arch in arches]
-            clauses.append('(' + ' OR '.join(archClause) + ')')
+            archPattern = r'\m(%s)\M' % '|'.join(arches)
+            clauses.append('arches ~ %(archPattern)s')
         if channelID is not None:
             channelID = get_channel_id(channelID, strict=True)
             joins.append('host_channels on host.id = host_channels.host_id')
